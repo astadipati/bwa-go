075fd1c04fc02ef1d6ddd8d5feff30e2c7856a5d
astadipati <astadipati@outlook.com>
2021-03-14 10:16:03 +0700

    simpan langsung dari repo

--- main.go
+++ main.go
@@ -3,42 +3,12 @@ package main
 import (
 	"bwastartup/user"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "mase:Kul0nuwun@tcp(127.0.0.1:3306)/bwa_startup?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Koneksi Ok")
-
-	// var users []user.User
-	// length := len(users)
-	// fmt.Println(length)
-
-	// db.Find(&users)
-	// length = len(users)
-	// fmt.Println(length)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("===============")
-	// }
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-
-}
-func handler(c *gin.Context) {
 	dsn := "mase:Kul0nuwun@tcp(127.0.0.1:3306)/bwa_startup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -46,8 +16,26 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	//kita panggil repo
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "Test simpan",
+	}
+	// test simpan dari repo
+	userRepository.Save(user)
 
-	c.JSON(http.StatusOK, users)
 }
+
+// func handler(c *gin.Context) {
+// 	dsn := "mase:Kul0nuwun@tcp(127.0.0.1:3306)/bwa_startup?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+// 	if err != nil {
+// 		log.Fatal(err.Error())
+// 	}
+
+// 	var users []user.User
+// 	db.Find(&users)
+
+// 	c.JSON(http.StatusOK, users)
+// }
